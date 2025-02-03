6f0fc9222229b3a0da40cbaac3e5d7b19e96493e
chaeO-Ov <oiocybew219@ewhain.net>
2025-02-04 00:53:08 +0900

    front for test

--- evision-backend/controllers/mainController.js
+++ evision-backend/controllers/mainController.js
@@ -1,4 +1,5 @@
 const getMainPage = async (req, res) => {
+    res.render('main');
     res.status(200).json({ message: "메인 페이지 데이터 제공" });
 }
 
--- evision-backend/controllers/recruitingController.js
+++ evision-backend/controllers/recruitingController.js
@@ -1,4 +1,5 @@
 const getRecruitingPage = async (req, res) => {
+    res.render('recruiting');
     res.status(200).json({ message: "모집 페이지 데이터 제공" });
 }
 
