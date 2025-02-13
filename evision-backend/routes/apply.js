e88191fd9927c81b753a29eaee76e8a6611190eb
chaeO-Ov <oiocybew219@ewhain.net>
2025-02-13 19:06:46 +0900

    프론트－백엔드 연결

--- evision-backend/routes/apply.js
+++ evision-backend/routes/apply.js
@@ -1,7 +1,9 @@
 import express from 'express';
-import { getApplyPage } from '../controllers/applyController.js';
+import { getApplyPage, postApply } from '../controllers/applyController.js';
+
 const router = express.Router();
 
 router.get('/', getApplyPage);
+router.post('/', postApply);
 
 export default router;
